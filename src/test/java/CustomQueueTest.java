e015330f8445c6ef56fff343c0922cc9af37d705
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-07 17:40:04 +0200

    Create testEnqueue method

--- src/test/java/CustomQueueTest.java
+++ src/test/java/CustomQueueTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomQueueTest {
 
+    @Test
+    void testEnqueue() {
+        CustomQueue customQueue = new CustomQueue();
+        customQueue.enqueue("test");
+
+        assertEquals("test", customQueue.peek());
+    }
 }
